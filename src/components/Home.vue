7581bedf9e921902eeb82404009b398fa9e783e3
ljbmyj <zhijiangtao66@yeah.net>
2020-03-26 21:40:49 +0800

    用户登陆与退出功能完成

---
+++ src/components/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <el-button @click="userLoginOut" type="info">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    userLoginOut () {
+      window.sessionStorage.clear()
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped></style>
--- src/main.js
+++ src/main.js
@@ -2,13 +2,18 @@ import Vue from 'vue'
 import './plugins/axios'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import './plugins/element.js'
+
+/* 导入全局样式表 */
+import './assets/global.css'
+/* 导入axios */
+/* import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios */
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
   render: h => h(App)
 }).$mount('#app')
